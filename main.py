51c2a5c096703846e305696aa71690a20291a503
Hyijun <32429581+Hyijun@users.noreply.github.com>
2018-11-09 21:45:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import urllib.request as ur
-
+import webbrowser
+import history_url
 
 
 def main():
-    title = 'https://en.wikipedia.org/wiki/' + input('请输入您要查找的enwiki标题')
+    title = input('请输入您要查找的enwiki标题')
     text = input('请输入需要查询的段落')
 
-    print(is_in_search(text, title))
+    list1 = history_url.get_url(title)
+    need_open_url = history_url.judge_history(list1, text, title)
+    webbrowser.open(need_open_url)
+    print('已打开链接：' + need_open_url)
 
 
 if __name__ == '__main__':
     main()
+    
